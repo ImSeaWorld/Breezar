2caf85420043a8c814f0666f515025994483194c
SeaWorld <robert.klitsch97@gmail.com>
2024-03-10 01:17:17 -0700

    Add Features component

---
+++ resources/js/Components/Features.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="features" >
+        <div class="container-3 row" style="justify-content: center;">
+            <div class="container-4 col-12 col-sm-8 col-md-3">
+                <div class="container-l-square">
+
+                    <svg width="42" height="42" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="EZ/ 1" filter="url(#filter0_d_215_2453)">
+                            <g clip-path="url(#clip0_215_2453)">
+                                <path id="Vector" fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M22 49C36.9117 49 49 36.9117 49 22C49 7.08833 36.9117 -5 22 -5C7.08828 -5 -5 7.08833 -5 22C-5 36.9117 7.08828 49 22 49ZM30.4231 7.57774C30.8331 6.12137 29.4197 5.26018 28.129 6.17976L10.1107 19.0159C8.71089 20.0132 8.93107 22 10.4415 22H15.1862V21.9633H24.4334L16.8986 24.6218L13.577 36.4223C13.167 37.8787 14.5801 38.7399 15.871 37.8202L33.8893 24.9842C35.2891 23.9869 35.0688 22 33.5586 22H26.3633L30.4231 7.57774Z"
+                                    fill="white" />
+                            </g>
+                            <rect x="0.5" y="0.5" width="43" height="43" rx="7.5" stroke="#EAD6FF" />
+                        </g>
+                        <defs>
+                            <filter id="filter0_d_215_2453" x="-2" y="-1" width="48" height="48"
+                                filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                <feColorMatrix in="SourceAlpha" type="matrix"
+                                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                <feOffset dy="1" />
+                                <feGaussianBlur stdDeviation="1" />
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_215_2453" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_215_2453"
+                                    result="shape" />
+                            </filter>
+                            <clipPath id="clip0_215_2453">
+                                <rect width="44" height="44" rx="8" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                </div>
+                <div class="text-6">
+                    <div class="title-2">Become a Pioneer</div>
+                    <p class="p">
+                        Your Handshake domain makes you a part of the growing effort to progressively decentralize the
+                        world
+                        wide web.
+                    </p>
+                </div>
+            </div>
+            
+            <div class="container-4 col-12 col-sm-8 col-md-3">
+                <div class="lock-wrapper">
+                    <!-- <img class="img-2" src="https://c.animaapp.com/crkyC7VV/img/lock-.svg" /> -->
+                    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="EZ/ 2" filter="url(#filter0_d_215_2484)">
+                            <rect x="0.5" y="0.5" width="43" height="43" rx="7.5" stroke="#BEDBFE" />
+                            <g id="lock$">
+                                <path id="Icon" fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M15.3333 16.6667C15.3333 12.9836 18.317 10 22 10C25.683 10 28.6667 12.9836 28.6667 16.6667V18H31.3333C33.3333 18 34 18.96 34 20.1333V32.8C34 34.2728 32.8061 35.4667 31.3333 35.4667H12.6667C11.1939 35.4667 10 34.2728 10 32.8V20.1333C10 18.96 10.6667 18 12.4667 18H15.3333V16.6667ZM26 16.6667V18H18V16.6667C18 14.4564 19.7897 12.6667 22 12.6667C24.2103 12.6667 26 14.4564 26 16.6667ZM22 22C20.5272 22 19.2587 23.1958 19.3333 24.6667C19.3827 25.6398 19.8784 26.401 20.6667 26.7755V28.6667C20.6667 29.219 20.9029 30.6667 22 30.6667C23.0971 30.6667 23.3333 29.219 23.3333 28.6667V26.7755C24.1216 26.401 24.6173 25.6398 24.6667 24.6667C24.7413 23.1958 23.4728 22 22 22Z"
+                                    fill="#2463EB" />
+                            </g>
+                        </g>
+                        <defs>
+                            <filter id="filter0_d_215_2484" x="-2" y="-1" width="48" height="48"
+                                filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                <feColorMatrix in="SourceAlpha" type="matrix"
+                                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                <feOffset dy="1" />
+                                <feGaussianBlur stdDeviation="1" />
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_215_2484" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_215_2484"
+                                    result="shape" />
+                            </filter>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="text-6">
+                    <div class="title-2">Stay Secure</div>
+                    <p class="p">
+                        Handshake domains are distributed on a blockchain, which provides more resistance to security
+                        scams
+                        and
+                        hacking attempts.
+                    </p>
+                </div>
+            </div>
+
+            <div class="container-4 col-12 col-sm-8 col-md-3">
+                <div class="sparkle-wrapper">
+                    <img class="img-2" src="https://c.animaapp.com/crkyC7VV/img/sparkle-.svg" />
+                </div>
+                <div class="text-6">
+                    <div class="title-2">Enjoy Online Freedom</div>
+                    <p class="p">
+                        As the web becomes less centralized, the power is shifted into the hands of domain name owners
+                        with
+                        minimal gatekeeping and oversight.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Features',
+    data() {
+        return {
+            features: [
+                {
+                    title: 'Become a Pioneer',
+                    description: 'Your Handshake domain makes you a part of the growing effort to progressively decentralize the world wide web.',
+                },
+                {
+                    title: 'Stay Secure',
+                    description: 'Handshake domains are distributed on a blockchain, which provides more resistance to security scams and hacking attempts.',
+                },
+                {
+                    title: 'Enjoy Online Freedom',
+                    description: 'As the web becomes less centralized, the power is shifted into the hands of domain name owners with minimal gatekeeping and oversight.',
+                }
+            ],
+        };
+    }
+}
+</script>
