af993e1303529be3e68110a84eeef543b6a12e8c
SeaWorld <robert.klitsch97@gmail.com>
2024-03-10 01:17:43 -0700

    Refactor Dashboard.vue component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <breeze-authenticated-layout :auth="auth">
+    <Authenticated :auth="auth">
         <template #header>
             <span class="text-h6 text-center">
                 Dashboard
@@ -7,15 +7,9 @@
         </template>
 
         <div class="q-py-md">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
+            <!--  -->
         </div>
-    </breeze-authenticated-layout>
+    </Authenticated>
 </template>
 
 <script>
@@ -25,7 +19,6 @@ export default {
     components: {
         Authenticated,
     },
-
     props: {
         auth: Object,
         errors: Object,
